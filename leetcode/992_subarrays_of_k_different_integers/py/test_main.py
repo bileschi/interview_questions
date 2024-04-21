884fbbdbd296cacd4d7e92e99ecd0c7bcb95a6f3
Stanley Bileschi <bileschi@google.com>
2024-04-21 00:05:49 -0400

    working but slow

---
+++ leetcode/992_subarrays_of_k_different_integers/py/test_main.py
@@ -0,0 +1,24 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+    def test_brute_force(self):
+        f = main.brute_force
+        self.generic_test(f)
+    
+    def test_brute_force(self):
+        f = main.brute_force
+        self.generic_test(f)
+    
+    def generic_test(self, f):
+        self.assertEqual(f([1,2,1,2,3], 2), 7)
+        self.assertEqual(f([1,2,1,3,4], 3), 3)
+        self.assertEqual(f([], 3), 0)
+        N = 100
+        big_array_of_ones = [1] * N
+        self.assertEqual(f(big_array_of_ones, 1), N * (N+1) / 2)
+
+
+
+if __name__ == '__main__':
+  unittest.main()
