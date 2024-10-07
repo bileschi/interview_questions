9ec85785d4f641dc7a694fa1d0edbc019e11714a
Stanley Bileschi <bileschi@google.com>
2024-10-07 14:11:17 -0400

    adds template directory for new lc script

---
+++ leetcode/template/py/main.py
@@ -0,0 +1,20 @@
+# Solution to leetcode problem $$DIGIT_GOES_HERE$$
+
+class Solution(object):
+ 
+    def countGen(self, gen):
+        count = 0
+        for dummy in gen:
+            count += 1
+        return(count)
+    
+
+    def heightChecker(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        s = sorted(heights)
+        g = ( (x,y) for (x,y) in zip(heights, s) if x != y)
+        return(self.countGen( g ))
+    
