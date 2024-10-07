11eb605495c41cb0f5eece6678be9bd712110d10
Stanley Bileschi <bileschi@google.com>
2024-10-07 14:11:17 -0400

    adds template directory for new lc script

---
+++ leetcode/template/py/test_main.py
@@ -0,0 +1,16 @@
+# Tests for leetcode problem $$DIGIT_GOES_HERE$$
+
+import main
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_basic(self):
+        s = main.Solution()
+        self.assertEqual(s.heightChecker([1,1,4,2,1,3]), 3)
+        self.assertEqual(s.heightChecker([5,1,2,3,4]), 5)
+        self.assertEqual(s.heightChecker([1,2,3,4,5]), 0)
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
