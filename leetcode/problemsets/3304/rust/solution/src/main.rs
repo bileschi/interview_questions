4eb1750a5090d26f99c009ac0f846e201118aacd
Stanley Bileschi <bileschi@google.com>
2024-10-11 11:31:02 -0400

    rust solution works... but does it suck?

--- leetcode/problemsets/3304/rust/solution/src/main.rs
+++ leetcode/problemsets/3304/rust/solution/src/main.rs
@@ -1,3 +1,48 @@
+struct Solution;
+
+// use std::collections::HashMap;
+
+
+impl Solution {
+    /* Build a Vec of the integer values expected at the
+    * kth position of the string.
+    */
+    
+    pub fn kth_character(k: i32) -> char {
+        let mut vec : Vec<u32> = Vec::<u32>::with_capacity(500);
+        vec.push(0 + 'a' as u32);
+        while vec.len() < 500 {
+            let prev_len = vec.len();
+            for i in 0..prev_len {
+                vec.push(1 + vec[i]);
+                if vec.len() == 500 {
+                    break;
+                }
+            }
+        }
+        let idx = usize::try_from(k-1).unwrap();
+        return char::from_u32(vec[idx]).unwrap();
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    for i in 1..11 {
+        println!("{:?}", Solution::kth_character(i));
+    }
+    // println!("{:?}", Solution::kth_character(1));
+    // println!("{:?}", Solution::kth_character(5));
+    // println!("{:?}", Solution::kth_character(10));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_normal_cases() {
+        assert_eq!(Solution::kth_character(5), 'b');
+        assert_eq!(Solution::kth_character(10), 'c');
+    }
+
 }
