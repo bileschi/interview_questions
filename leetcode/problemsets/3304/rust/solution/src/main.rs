9f9e9ee9e47c7c8d5dd22a0dcc4b450e4f727bfe
Stanley Bileschi <bileschi@google.com>
2024-10-11 11:37:31 -0400

    better rust solution for 3304

--- leetcode/problemsets/3304/rust/solution/src/main.rs
+++ leetcode/problemsets/3304/rust/solution/src/main.rs
@@ -1,38 +1,44 @@
 struct Solution;
 
-// use std::collections::HashMap;
+fn increment_char(c: char) -> char {
+    let mut val = c as u32;
+    val += 1;
+    if val > 'z' as u32 {
+        val = 'a' as u32;
+    }
+    return char::from_u32(val).unwrap();
+}   
 
 
 impl Solution {
     /* Build a Vec of the integer values expected at the
     * kth position of the string.
+    *
+    * TODO: find a way to build this vector one time 
+    * outside of the solution, rather than on each invocation.
     */
     
     pub fn kth_character(k: i32) -> char {
-        let mut vec : Vec<u32> = Vec::<u32>::with_capacity(500);
-        vec.push(0 + 'a' as u32);
+        let mut vec : Vec<char> = Vec::<char>::with_capacity(500);
+        vec.push(char::from_u32('a' as u32).unwrap());
         while vec.len() < 500 {
             let prev_len = vec.len();
             for i in 0..prev_len {
-                vec.push(1 + vec[i]);
+                vec.push(increment_char(vec[i]));
                 if vec.len() == 500 {
                     break;
                 }
             }
         }
         let idx = usize::try_from(k-1).unwrap();
-        return char::from_u32(vec[idx]).unwrap();
+        return vec[idx];
     }
 }
 
 fn main() {
-    println!("Hello, world!");
     for i in 1..11 {
         println!("{:?}", Solution::kth_character(i));
     }
-    // println!("{:?}", Solution::kth_character(1));
-    // println!("{:?}", Solution::kth_character(5));
-    // println!("{:?}", Solution::kth_character(10));
 }
 
 #[cfg(test)]
