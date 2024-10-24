905caf7dd28135e14ecf45871b6267b270bd6b23
Stanley Bileschi <bileschi@google.com>
2024-10-24 15:37:12 -0400

    2392 (#23)
    * add 2392
    
    * adds bash script for creating leetcode directory python & rust (#17)
    
    * adds newline
    
    * adds template directory for new lc script
    
    * python version 3304 (#18)
    
    * clean up template & script (#19)
    
    * rust solution works... but does it suck?
    
    * better rust solution for 3304
    
    * 2641 (#20)
    
    * adds py solution to 2641.  Seems to work but exceeds memory for leetcode
    
    * this version passes leetcode
    
    * Extends tests to cover both solutions
    
    * Clean up and reformat.
    
    * Add faster cleaner Solution 3
    
    * fix debug logging
    
    * Adds rust solution (8ms)
    
    * 0951 (#21)
    
    * add python solution
    
    * adds test from problem description
    
    * add rust
    
    * 0951 (#21) (#22)
    
    * add python solution
    
    * adds test from problem description
    
    * add rust
    
    * Working accepted py solution.

--- leetcode/problemsets/2392/py/main.py
+++ leetcode/problemsets/2392/py/main.py
@@ -1,25 +1,86 @@
 import collections
+from dataclasses import dataclass, field
+from typing import List, Dict, Optional, Set
+
+@dataclass
+class GraphNode:
+    id: int
+    child_idxs: Set[int] = field(default_factory=set)
+    parent_idxs: Set[int] = field(default_factory=set)
+
+@dataclass
+class Graph:
+    k: int
+    idx_to_node: Dict[int, GraphNode]
+    # Maintain a collection of nodes with no parents – for topological sort.
+    idx_no_parent: Set[int]
+
+DEBUG = False
+def dbprint(*args, **kwargs):
+    if DEBUG:
+        print(*args, **kwargs)
+
+def build_graph(k: int, edges: List[List[int]]) -> Graph:
+    idx_to_node = {}
+    for i in range(1,k+1):
+        idx_to_node[i] = GraphNode(i)
+    for [before_idx, after_idx] in edges:
+        # after node is a child of before node
+        idx_to_node[before_idx].child_idxs.add(after_idx)
+        # before node is a parent of after node
+        idx_to_node[after_idx].parent_idxs.add(before_idx)
+    idx_no_parent = set([n.id for n in idx_to_node.values() if not n.parent_idxs])
+    return Graph(k, idx_to_node, idx_no_parent)
+    
+def remove_node(node_idx: int, graph: Graph) -> Optional[GraphNode]:
+    if node_idx not in graph.idx_to_node:
+        return None
+    n = graph.idx_to_node.pop(node_idx)
+    # All this node's parents need to remove a child.
+    for p_idx in n.parent_idxs:
+        p = graph.idx_to_node[p_idx]
+        p.child_idxs.remove(node_idx)
+    # All this node's children need to remove a parent.
+    # This may orphan some children.
+    for c_idx in n.child_idxs:
+        c = graph.idx_to_node[c_idx]
+        c.parent_idxs.remove(node_idx)
+        if not c.parent_idxs:
+            graph.idx_no_parent.add(c_idx)
+    # If this node was in the no-parent set, remove it.
+    graph.idx_no_parent.discard(node_idx)
+    return n
 
 
-class Graph(object):
-    def __init__(self, k, edges):
-        self.k = k
-        self.edges = edges
-        self.source_nodes = set(i for [i, _] in edges)
+def valid_order(graph: Graph) -> Optional[List[int]]:
+    # Collect the set of nodes that have no parents.
+    # Remove these from the graph, in any order.
+    # If we end up with a cycle, return None.
+    #
+    # Destroys graph
+    ordered_idxs = []
+    while graph.idx_no_parent:
+        # Necessary to prevent modifying the set while iterating.
+        copy_idx_no_parent = list(graph.idx_no_parent)
+        for idx in copy_idx_no_parent:
+            dbprint(f"Removing node {idx}")
+            n = remove_node(idx, graph)
+            ordered_idxs.append(n.id)
+    # If there are still elements in the graph, then there is a cycle.
+    if graph.idx_to_node:
+        return None
+    return ordered_idxs
 
-    def has_loops(self):
-        self.grey_nodes = set(self.source_nodes)
-        for n in self.grey_nodes:
-            if self.has_loop_from(n):
-                return True
-            
-    def has_loop_from(self, node):
-        self.visited = [False] * self.k
-        self.stack = []
-        self.visited[node] = True
+def build_matrix(row_order: List[int], col_order: List[int]) -> List[List[int]]:
+    k = len(row_order)
+    val_to_row = {v: i for (i, v) in enumerate(row_order)}
+    val_to_col = {v: i for (i, v) in enumerate(col_order)}
+    matrix = [[0 for _ in range(k)] for _ in range(k)]
+    dbprint(matrix)
+    for i in range(1, k+1):
+        matrix[val_to_row[i]][val_to_col[i]] = i
+    return matrix
 
-        for [i, j] in self.edges:
-            if self.visited
 
 class Solution(object):
     def buildMatrix(self, k, rowConditions, colConditions):
@@ -29,27 +90,17 @@ class Solution(object):
         :type colConditions: List[List[int]]
         :rtype: List[List[int]]
         """
-        if (not self.validConditions(rowConditions, k) or 
-            not self.validConditions(colConditions, k)):
+        row_graph = build_graph(k, rowConditions)
+        row_order = valid_order(row_graph)
+        if row_order is None:
             return []
+        col_graph = build_graph(k, colConditions)
+        col_order = valid_order(col_graph)
+        if col_order is None:
+            return []
+        matrix = build_matrix(row_order, col_order)
+        return matrix
 
-    def validConditions(self, conditions, k):
-        # Condtions are invalid if it contains any value greater than k
-        # Condtions are invalid if it contains any value less than 1
-        for [i, j] in conditions:
-            if i == j:
-                return False
-            if i > k or j > k:
-                return False
-            if i < 1 or j < 1:
-                return False
-        # Conditions are invalid if they form a loop.
-        # Treat conditions as a directed graph.  Check the DAG for loops.
-        # Fail if loop.
-        graph = Graph(k, conditions)
-        if graph.has_loops():
-            return False
-        return True
 
 
 """
--- leetcode/problemsets/2392/py/test_main.py
+++ leetcode/problemsets/2392/py/test_main.py
@@ -2,34 +2,120 @@ import main
 import unittest
 
 class TestMain(unittest.TestCase):
-    def test_tests_run(self):
-        s = main.Solution()
-        print("Tests run")
-        self.assertTrue(True)
 
-    def xtest_valid_conditions(self):
-        s = main.Solution()
-        self.assertTrue(s.validConditions([], 10))
-        self.assertTrue(s.validConditions([[1, 2]], 2))
-        self.assertTrue(s.validConditions([[1, 2], [2, 3], [2, 4]], 10))
-        self.assertTrue(s.validConditions([[1, 2], [2, 3], [3, 4]], 10))
-        self.assertTrue(s.validConditions([[4, 3], [3, 2], [2, 1]], 10))
-                        
-    def xtest_valid_conditions_bad(self):
-        s = main.Solution()
-        # condition greater than k
-        self.assertFalse(s.validConditions([[1, 3]], 2))
-        self.assertFalse(s.validConditions([[3, 1]], 2))
-        # condition less than 1
-        self.assertFalse(s.validConditions([[1, 2], [0, 2]], 10))
-        self.assertFalse(s.validConditions([[1, 2], [2, 0]], 10))
-        # bad partial ordering
-        self.assertFalse(s.validConditions([[1, 2], [2, 3], [3, 1]], 10))
+    def test_build_empty_graph(self):
+        g = main.build_graph(0, [])
+        self.assertEqual(g.k, 0)
+        self.assertEqual(g.idx_to_node, {})
+        self.assertCountEqual(g.idx_no_parent, set())
+        # Topo sort
+        s = main.valid_order(g)
+        self.assertEqual(s, [])
 
-    def test_valid_conditions_bad(self):
+    def test_build_graph_one_element(self):
+        g = main.build_graph(1, [])
+        self.assertEqual(g.k, 1)
+        self.assertCountEqual(g.idx_to_node.keys(), [1])
+        self.assertCountEqual(g.idx_no_parent, set([1]))
+        # Topo sort
+        s = main.valid_order(g)
+        self.assertEqual(s, [1])
+
+    def test_remove_node(self):
+        g = main.build_graph(1, [])
+        self.assertEqual(g.k, 1)
+        self.assertCountEqual(g.idx_to_node.keys(), [1])
+        self.assertCountEqual(g.idx_no_parent, set([1]))
+        n = main.remove_node(1, g) 
+        self.assertEqual(g.k, 1)
+        self.assertCountEqual(g.idx_to_node.keys(), [])
+        self.assertCountEqual(g.idx_no_parent, set([]))
+        self.assertEqual(n.id, 1)
+
+    def test_build_graph_two_elements_no_edge(self):
+        g = main.build_graph(2, [])
+        self.assertEqual(g.k, 2)
+        self.assertCountEqual(g.idx_to_node.keys(), [1, 2])
+        self.assertCountEqual(g.idx_no_parent, set([1, 2]))
+        # Topo sort
+        s = main.valid_order(g)
+        self.assertCountEqual(s, [1, 2])
+
+    def test_build_graph_two_elements_one_edge_1(self):
+        g = main.build_graph(2, [[1, 2]])
+        self.assertEqual(g.k, 2)
+        self.assertCountEqual(g.idx_to_node.keys(), [1, 2])
+        self.assertCountEqual(g.idx_no_parent, set([1]))
+        # Topo sort
+        s = main.valid_order(g)
+        self.assertEqual(s, [1, 2])
+
+    def test_build_graph_two_elements_one_edge_2(self):
+        g = main.build_graph(2, [[2, 1]])
+        self.assertEqual(g.k, 2)
+        self.assertCountEqual(g.idx_to_node.keys(), [1, 2])
+        self.assertCountEqual(g.idx_no_parent, set([2]))
+        # Topo sort
+        s = main.valid_order(g)
+        self.assertEqual(s, [2, 1])
+
+    def test_build_graph_two_elements_with_loop(self):
+        g = main.build_graph(2, [[2, 1], [1, 2]])
+        self.assertEqual(g.k, 2)
+        self.assertCountEqual(g.idx_to_node.keys(), [1, 2])
+        self.assertCountEqual(g.idx_no_parent, set())
+        # No valid topo sort
+        s = main.valid_order(g)
+        self.assertEqual(s, None)
+
+    def test_build_graph_leetcode_ex(self):
+        g = main.build_graph(3, [[1, 2], [3, 2]])
+        self.assertEqual(g.k, 3)
+        self.assertCountEqual(g.idx_to_node.keys(), [1, 2, 3])
+        self.assertCountEqual(g.idx_no_parent, set([1, 3]))
+        # Topo sort
+        s = main.valid_order(g)
+        self.assertCountEqual(s, [1,2,3])
+        self.assertEqual(s[2], 2)
+
+    def test_build_matrix_1(self):
+        row_order = [1, 2, 3]
+        col_order = [1, 2, 3]
+        m = main.build_matrix(row_order, col_order)
+        row_1 = m[0]
+        row_2 = m[1]
+        row_3 = m[2]
+        self.assertEqual(row_1, [1, 0, 0])
+        self.assertEqual(row_2, [0, 2, 0])
+        self.assertEqual(row_3, [0, 0, 3])
+
+    def test_build_matrix_2(self):
+        row_order = [1, 2, 3]
+        col_order = [3, 2, 1]
+        m = main.build_matrix(row_order, col_order)
+        row_1 = m[0]
+        row_2 = m[1]
+        row_3 = m[2]
+        self.assertEqual(row_1, [0, 0, 1])
+        self.assertEqual(row_2, [0, 2, 0])
+        self.assertEqual(row_3, [3, 0, 0])
+
+
+    def test_solution_1(self):
         s = main.Solution()
-        self.assertFalse(s.validConditions([[1, 2], [2, 3], [3, 1]], 10))
-                        
+        row_cond = [[1, 2], [3, 2]]
+        col_cond = [[2, 1], [3, 2]]
+        k = 3
+        mtx = s.buildMatrix(k, row_cond, col_cond)
+        self.assertEqual(mtx[2][1], 2)
+
+    def test_solution_2(self):
+        s = main.Solution()
+        row_cond = [[1,2],[2,3],[3,1],[2,3]]
+        col_cond = [[2,1]]
+        k = 3
+        mtx = s.buildMatrix(k, row_cond, col_cond)
+        self.assertEqual(mtx, [])                        
                         
 
 if __name__ == '__main__':
